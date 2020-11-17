be427015ca4a10be7c8a2611f0b261f681aab37b
LamprosKossivakis <74597965+LamprosKossivakis@users.noreply.github.com>
2020-11-17 13:55:02 +0200

    Add files via upload

---
+++ Testcovid.java
@@ -0,0 +1,19 @@
+import java.text.ParseException;
+import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+public class Testcovid {
+	public static void main(String[]args) throws ParseException {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Δώσε ονοματεπώνυμο:");
+		String name = scanner.next();
+		System.out.println("Δώσε ημερομηνία γέννησης:");
+		String pattern = "dd/MM/yyyy";
+		SimpleDateFormat format = new SimpleDateFormat(pattern);
+        String d = scanner.next();
+        Date date = format.parse(d);
+        Testcovid patient = new Testcovid(name,date);
+        System.out.println(patient.getName() + " getting tested");
+	}
+	
+}
