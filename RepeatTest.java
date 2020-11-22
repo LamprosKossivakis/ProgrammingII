110d46108a0ca1fb459be20056b2dd25e267a89b
natalia-poulakida1 <72792245+natalia-poulakida1@users.noreply.github.com>
2020-11-22 12:38:45 +0200

    Add files via upload

--- RepeatTest.java
+++ RepeatTest.java
@@ -2,12 +2,17 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class RepeatTest {
+
+	// the methods print all the negative and positive patients that need to be tested today
+	// we can transfer them to Positive and Negative  
 	
-	public static void PositiveSub() {
-		Date date = new Date(System.currentTimeMillis()); //today's date
+	public static void RepeatPositive() {
+		//today's date
+		Date date = new Date(System.currentTimeMillis()); 
 		System.out.println("Positive patients that need to repeat test: ");
 		for (int i=0;i<positivePatients.length; i++) {
 			if (positivePatients[i]!=null) {
+				//difference between today's date and the day of the test
 				long diff1 = (date.getTime()-positivePatients[i].//getDate().getTime());
 				long diff = TimeUnit.DAYS.convert(diff1, TimeUnit.MILLISECONDS);
 				if (diff = 15) {
@@ -16,15 +21,16 @@ public class RepeatTest {
 			}
 		}
 	}
-	
-	public static void NegativeSub() {
-		Date date = new Date(System.currentTimeMillis()); //today's date
+
+	public static void RepeatNegative() {
+		//today's date
+		Date date = new Date(System.currentTimeMillis()); 
 		System.out.println("Negative patients that need to repeat test: ");
 		for (int i=0;i<negativePatients.length; i++) {
 			if (negativePatients[i]!=null) {
+				//difference between today's date and the day of the test
 				long diff1 = (date.getTime()-negativePatients[i].//getDate().getTime());
-				long diff = TimeUnit.DAYS.convert(diff1, TimeUnit.MILLISECONDS); //difference between today's date
-				// and the day of the test
+				long diff = TimeUnit.DAYS.convert(diff1, TimeUnit.MILLISECONDS); 
 				if (diff = 3) {
 					System.out.println (negativePatients[i]);
 				}
