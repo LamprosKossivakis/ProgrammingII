2c813d47244cefc8eb106697b87291412db651e7
Chariton Kaltsidis <62574352+AporRiToN@users.noreply.github.com>
2020-12-05 13:15:06 +0200

    Update RepeatTest.java

--- RepeatTest.java
+++ RepeatTest.java
@@ -1,4 +1,3 @@
-package com.company;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
@@ -9,16 +8,17 @@ public class RepeatTest {
 	
 	public static void RepeatPositive() {
 		//today's date
-		Date date = new Date(System.currentTimeMillis()); 
+		Date date = new Date(System.currentTimeMillis());
+
 		System.out.println("Positive patients that need to repeat test: ");
-		for (int i=0;i<positivePatients.length; i++) {
-			if (positivePatients[i]!=null) {
+		for (int i=0;i<Positive.positivePatients.length; i++) {
+			if (Positive.positivePatients[i]!=null) {
 				//difference between today's date and the day of the test
-				long diff1 = (date.getTime()-positivePatients[i].//getDate().getTime());
+				long diff1 = (date.getTime()-Positive.positivePatients[i];   	//getDate().getTime());
 				long diff = TimeUnit.DAYS.convert(diff1, TimeUnit.MILLISECONDS);
-				if ((diff = 15) && (postiveTestsDone[i]==1)){
-					System.out.println (positivePatients[i]);
-					positiveTestsDone[i]=2;
+				if ((diff = 15) && (Positive.positiveTestsDone[i]==1)){
+					System.out.println (Positive.positivePatients[i]);
+					Positive.positiveTestsDone[i]=2;
 				}
 			}
 		}
@@ -28,14 +28,14 @@ public class RepeatTest {
 		//today's date
 		Date date = new Date(System.currentTimeMillis()); 
 		System.out.println("Negative patients that need to repeat test: ");
-		for (int i=0;i<negativePatients.length; i++) {
-			if (negativePatients[i]!=null) {
+		for (int i=0;i<Negative.negativePatients.length; i++) {
+			if (Negative.negativePatients[i]!=null) {
 				//difference between today's date and the day of the test
-				long diff1 = (date.getTime()-negativePatients[i].//getDate().getTime());
+				long diff1 = (date.getTime()-Negative.negativePatients[i];	//getDate().getTime());
 				long diff = TimeUnit.DAYS.convert(diff1, TimeUnit.MILLISECONDS); 
-				if ((diff = 3) && (negativeTestsDone[i]==1)) {
-					System.out.println (negativePatients[i]);
-					negativeTestsDone[i]=2;
+				if ((diff = 3) && (Negative.negativeTestsDone[i]==1)) {
+					System.out.println (Negative.negativePatients[i]);
+					Negative.negativeTestsDone[i]=2;
 				}
 			}
 		}
